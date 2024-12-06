7ae0db08dce6e806ad72244c49ce29620f947ca7
ZLMTech <127011480+MrDevelop636@users.noreply.github.com>
2024-12-06 20:02:23 +0100

    Update framework.js

--- framework.js
+++ framework.js
@@ -1,1 +1,6 @@
 // Author: ZLMTech | Version: BETA |
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = 'main.css'; 
+document.head.appendChild(link);
