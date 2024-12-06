a4f3473350f17401ca5b0825f28b0e1f15a3531d
ZLMTech <127011480+MrDevelop636@users.noreply.github.com>
2024-12-06 21:18:38 +0100

    Update framework.js

--- framework.js
+++ framework.js
@@ -7,3 +7,10 @@ link.href = 'main.css';
 document.head.appendChild(link);
 
 
+document.querySelectorAll('[class^="s-"]').forEach(element => {
+const sizeClass =      Array.from(element.classList).find(cls => cls.startsWith('s-'));
+if (sizeClass) {
+const size = sizeClass.replace('s-', '');
+element.style.fontSize = `${size}px`;
+    }
+});
