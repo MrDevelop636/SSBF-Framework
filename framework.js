5416a221eb4ae98f35a005fd96c8019812fe2e17
ZLMTech <127011480+MrDevelop636@users.noreply.github.com>
2024-12-06 21:14:05 +0100

    Update framework.js

--- framework.js
+++ framework.js
@@ -1,6 +1,9 @@
 // Author: ZLMTech | Version: BETA |
+// add link to Basic css file
 const link = document.createElement('link');
 link.rel = 'stylesheet';
 link.type = 'text/css';
 link.href = 'main.css'; 
 document.head.appendChild(link);
+
+
